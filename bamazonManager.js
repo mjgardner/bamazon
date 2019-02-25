ab3f1e9c4cc5761e9f1deffad69998222f444335
Mark Gardner <mark.gardner@endurance.com>
2019-02-25 17:34:40 -0600

    WIP first cut at challenge #2

---
+++ bamazonManager.js
@@ -0,0 +1,143 @@
+require("dotenv").config();
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const { table } = require("table");
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  switchboard();
+});
+
+function switchboard() {
+  inquirer
+    .prompt([
+      {
+        name: "menu",
+        type: "list",
+        message: "Menu:",
+        choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product",
+          "Quit"
+        ]
+      }
+    ])
+    .then(function(answers) {
+      switch (answers.menu) {
+        case "View Products for Sale":
+          viewProducts();
+          break;
+        case "View Low Inventory":
+          viewLowInventory();
+          break;
+        case "Add to Inventory":
+          addInventory();
+          break;
+        case "Add New Product":
+          addProduct();
+          break;
+        case "Quit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+function viewProducts() {
+  connection.query(
+    "select item_id, product_name, price, stock_quantity from products",
+    function(err, res) {
+      if (err) throw err;
+      outputTable(res);
+      switchboard();
+    }
+  );
+}
+
+function viewLowInventory() {
+  connection.query(
+    "select item_id, product_name, price, stock_quantity from products where stock_quantity < 5",
+    function(err, res) {
+      if (err) throw err;
+      if (res.length > 0) {
+        outputTable(res);
+      } else {
+        console.log("All items have at least a stock quantity of 5!");
+      }
+      switchboard();
+    }
+  );
+}
+
+function outputTable(response) {
+  var output = table(
+    response.map(function(rec) {
+      return [
+        rec.item_id,
+        rec.product_name,
+        "$" + parseFloat(rec.price).toFixed(2),
+        rec.stock_quantity
+      ];
+    }),
+    {
+      columns: {
+        0: { alignment: "right" },
+        2: { alignment: "right" },
+        3: { alignment: "right" }
+      }
+    }
+  );
+  console.log(output);
+}
+
+function addInventory() {
+  connection.query(
+    "select item_id, product_name, stock_quantity from products",
+    function(err, res) {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: "item_id",
+            message: "Select a product to add more inventory:",
+            type: "list",
+            choices: res.map(function(record) {
+              return { name: record.product_name, value: record.item_id };
+            })
+          }
+        ])
+        .then(function(itemAnswers) {
+          inquirer
+            .prompt([
+              {
+                name: "add_quantity",
+                message: "How many more of this item would you like to add?"
+              }
+            ])
+            .then(function(qtyAnswers) {
+              connection.query(
+                "update products set stock_quantity = stock_quantity + ? where item_id = ?",
+                [
+                  parseInt(qtyAnswers.add_quantity),
+                  parseInt(itemAnswers.item_id)
+                ],
+                function(err, res) {
+                  
+                });
+              switchboard();
+            });
+        });
+    }
+  );
+}
+
+function addProduct() {}
